ef2cf1048eb8e45f3e19c72de031a72cec55c131
Andrew Grieve <agrieve@chromium.org>
2013-07-31 19:57:24 -0400

    [CB-4417] Move cordova-plugin-media-capture to its own Java package.

--- src/android/Capture.java
+++ src/android/Capture.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package org.apache.cordova.core;
+package org.apache.cordova.mediacapture;
 
 import java.io.File;
 import java.io.FileInputStream;
