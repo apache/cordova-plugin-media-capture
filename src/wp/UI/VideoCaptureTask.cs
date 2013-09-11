0cc5aee09822179b1dcda3a9a84152377deaa785
Yangtb <yangtb@polyvi.com>
2013-09-11 17:32:49 +0800

    fixed cordova cli add capture plugin not work wp
    1. updated feature name in plugin.xml;
    2. updated navigate VideoRecorder.xaml & AudioRecorder.xaml Uri;

--- src/wp/UI/VideoCaptureTask.cs
+++ src/wp/UI/VideoCaptureTask.cs
@@ -74,7 +74,7 @@ namespace WPCordovaClassLib.Cordova.UI
 
                 root.Navigated += new System.Windows.Navigation.NavigatedEventHandler(NavigationService_Navigated);
 
-                string baseUrl = WPCordovaClassLib.Cordova.Commands.BaseCommand.GetBaseURL();
+                string baseUrl = "/";
                 // dummy parameter is used to always open a fresh version
                 root.Navigate(new System.Uri(baseUrl + "CordovaLib/UI/VideoRecorder.xaml?dummy=" + Guid.NewGuid().ToString(), UriKind.Relative));
             });
