5fb49175c108363f061f462faf3280cc1fb434a0
Larry Bahr <larrybahr@users.noreply.github.com>
2018-08-20 12:29:40 -0500

    tab to space fix

--- types/index.d.ts
+++ types/index.d.ts
@@ -123,7 +123,7 @@ declare var CaptureError: {
     CAPTURE_INVALID_ARGUMENT: number;
     CAPTURE_NO_MEDIA_FILES: number;
     CAPTURE_NOT_SUPPORTED: number;
-	 CAPTURE_PERMISSION_DENIED: number;
+    CAPTURE_PERMISSION_DENIED: number;
 }
 
 /** Encapsulates audio capture configuration options. */
